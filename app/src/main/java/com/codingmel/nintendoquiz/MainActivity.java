397701dbb230b2098a12771db3d284fd36704373
Melanie V <melanie.vaubien@hotmail.com>
2020-03-18 14:23:15 +0100

    Animation Bakground
    Add Library for animation Background in the menu

--- app/src/main/java/com/codingmel/nintendoquiz/MainActivity.java
+++ app/src/main/java/com/codingmel/nintendoquiz/MainActivity.java
@@ -3,12 +3,26 @@ package com.codingmel.nintendoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.q42.android.scrollingimageview.ScrollingImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+        ScrollingImageView scrollingBackground = findViewById(R.id.bgImageView);
+        scrollingBackground.stop();
+        scrollingBackground.start();
+        /*ImageView backgroundImage = findViewById(R.id.bgImageView);
+        backgroundImage.setImageResource(R.drawable.nintendo_characters);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.defilement_loop);
+        backgroundImage.startAnimation(animation);*/
     }
 }
