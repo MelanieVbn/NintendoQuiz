b71c76b6f76e5266d1c20453efdd69c02ca5134e
Melanie V <melanie.vaubien@hotmail.com>
2020-03-19 20:22:05 +0100

    Add Img and Sound to Questions

--- app/src/main/java/com/codingmel/nintendoquiz/MainActivity.java
+++ app/src/main/java/com/codingmel/nintendoquiz/MainActivity.java
@@ -66,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
                                 intent.putExtra("difficulty","MOYEN");
                                 break;
                             case 2:
-                                intent.putExtra("questions",getQuestionsByDifficulty("FACILE"));
-                                intent.putExtra("difficulty","MOYEN");
+                                intent.putExtra("questions",getQuestionsByDifficulty("DIFFICILE"));
+                                intent.putExtra("difficulty","DIFFICILE");
                                 break;
                         }
                         mp.stop();
@@ -103,27 +103,27 @@ public class MainActivity extends AppCompatActivity {
                 new Question("Quel est le nom de ce personnage ?", "FACILE", R.drawable.dr_mario, 0, Arrays.asList("Professeur Mario", "Mario le Toubib", "Dr.Mario"), "Dr.Mario"),
                 new Question("Quelle console avait cette intoduction ?", "FACILE", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Game Cube", "Wii", "Nintendo 64"), "Game Cube"),
 
-                new Question("Qui est cet homme ?", "MOYEN", R.drawable.red_mushroom, 0, Arrays.asList("Shigeru Miyamoto", "Saturo Iwata", "Hayao Miyazaki"), "Shigeru Miyamoto"),
-                new Question("Quel est le rôle de Marie dans Animal Crossing ?", "MOYEN", 0, R.raw.gamecube_intro, Arrays.asList("Elle est l'assistante du maire", "Elle est le maire", "Elle travaille dans une boutique"), "Elle est l'assistante du maire"),
-                new Question("De quelle firme venait Sonic avant d'apparaitre aux côté de Mario ?", "MOYEN", 0, R.raw.gamecube_intro, Arrays.asList("Sega", "Il a toujours été chez Nintendo", "DreamWorks"), "Sega"),
-                new Question("Quel est le nom de ce personnage ?", "MOYEN", 0, R.raw.gamecube_intro, Arrays.asList("Kirby", "Rondoudou", "Mrs.PacMan"), "Kirby"),
-                new Question("Quel est le nom du royaume d'où vient la princesse Zelda ?", "MOYEN", 0, R.raw.gamecube_intro, Arrays.asList("Hyrule", "Royaume Champignon", "Narnia"), "Hyrule"),
-                new Question("De quel jeu vient cette musique ?", "MOYEN", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Animal Crossing", "Pokemon", "Zelda"), "Animal Crossing"),
-                new Question("De quel effet vient cette musique (Mario Bros) ?", "MOYEN", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Quand une étoile est récupérée", "Quand un champignon est récupéré", "Quand un drapeau est passé"), "Quand une étoile est récupérée"),
-                new Question("A quel moment cette musique est-elle jouée (Zelda) ?", "MOYEN", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Quand un coffre est ouvert", "Quand l'épée de légende est récupérée", "Quand un pouvoir est acquis"), "Quand un coffre est ouvert"),
-                new Question("De quel jeu Smash Bros vient cette musique ?", "MOYEN", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Super Smash Bros Ultimate", "Super Smash Bros Melee", "Supe Smash Bros Brawl"), "Super Smash Bros Ultimate"),
-                new Question("Quelle console avait cette intoduction ?", "MOYEN", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("La GameBoy Color", "La DS", "La Wii"), "La GameBoy Color"),
+                new Question("Qui est cet homme ?", "MOYEN", R.drawable.shigeru_miyamot, 0, Arrays.asList("Shigeru Miyamoto", "Saturo Iwata", "Hayao Miyazaki"), "Shigeru Miyamoto"),
+                new Question("Quel est le rôle de Marie dans Animal Crossing ?", "MOYEN", R.drawable.marie, 0,Arrays.asList("Elle est l'assistante du maire", "Elle est le maire", "Elle travaille dans une boutique"), "Elle est l'assistante du maire"),
+                new Question("De quelle firme venait Sonic avant d'apparaitre aux côté de Mario ?", "MOYEN", R.drawable.sonic, 0, Arrays.asList("Sega", "Il a toujours été chez Nintendo", "DreamWorks"), "Sega"),
+                new Question("Quel est le nom de ce personnage ?", "MOYEN", R.drawable.kirby, 0, Arrays.asList("Kirby", "Rondoudou", "Mrs.PacMan"), "Kirby"),
+                new Question("Quel est le nom du royaume d'où vient la princesse Zelda ?", "MOYEN", R.drawable.hyrule, 0, Arrays.asList("Hyrule", "Royaume Champignon", "Narnia"), "Hyrule"),
+                new Question("De quel jeu vient cette musique ?", "MOYEN", R.drawable.red_mushroom, R.raw.animal_crossing_wild_world_theme, Arrays.asList("Animal Crossing", "Pokemon", "Zelda"), "Animal Crossing"),
+                new Question("De quel effet vient cette musique (Mario Bros) ?", "MOYEN", R.drawable.red_mushroom, R.raw.mario_bros_star_theme, Arrays.asList("Quand une étoile est récupérée", "Quand un champignon est récupéré", "Quand un drapeau est passé"), "Quand une étoile est récupérée"),
+                new Question("A quel moment cette musique est-elle jouée (Zelda) ?", "MOYEN", R.drawable.red_mushroom, R.raw.zelda_chest_opening, Arrays.asList("Quand un coffre est ouvert", "Quand l'épée de légende est récupérée", "Quand un pouvoir est acquis"), "Quand un coffre est ouvert"),
+                new Question("De quel jeu Smash Bros vient cette musique ?", "MOYEN", R.drawable.red_mushroom, R.raw.ssbu_main_theme, Arrays.asList("Super Smash Bros Ultimate", "Super Smash Bros Melee", "Supe Smash Bros Brawl"), "Super Smash Bros Ultimate"),
+                new Question("Quelle console avait cette intoduction ?", "MOYEN", R.drawable.red_mushroom, R.raw.gameboy_startup, Arrays.asList("La GameBoy Color", "La DS", "La Wii"), "La GameBoy Color"),
 
-                new Question("Quel était le premier nom de Mario ?", "DIFFICILE", R.drawable.red_mushroom, 0, Arrays.asList("JumpMan", "Il s'est toujours appelé Mario", "Luigi"), "JumpMan"),
-                new Question("Quel est le nom de cette console ?", "DIFFICILE", R.drawable.red_mushroom, 0, Arrays.asList("Famicom", "MegaDrive", "Nintendo 64"), "Famicom"),
-                new Question("Qui se cache sous cette armure ?", "DIFFICILE", R.drawable.red_mushroom, 0, Arrays.asList("Samus", "Luna", "Megaman"), "Samus"),
-                new Question("Au départ, que vendant Nintendo ?", "DIFFICILE", R.drawable.red_mushroom, 0, Arrays.asList("Des jeux de cartes", "Des maquettes de voitures", "Des figurines"), "Des jeux de cartes"),
-                new Question("Qui a developpé les jeux Megaman ?", "DIFFICILE", R.drawable.red_mushroom, 0, Arrays.asList("Capcom", "Nintendo", "Sega"), "Game Cube"),
-                new Question("De quel niveau de Mario Kart 64 vient cette musique ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Route Arc-en-ciel", "Circuit Mario", "Parc Baby"), "Route Arc-en-ciel"),
-                new Question("", "DIFFICILE", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Game Cube", "Wii", "Nintendo 64"), "Game Cube"),
-                new Question("De quel jeu vient cette musique ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Megaman", "Wii", "Nintendo 64"), "Game Cube"),
-                new Question("Qui a composé l'ost des jeux a licence Zelda ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("Koji Kondo", "Asuka Ota", "Junichi Masuda"), "Koji Kondo"),
-                new Question("Quelle console avait cette intoduction ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.gamecube_intro, Arrays.asList("La Nes", "La Nintendo 64", "Les premières versions de la GameBoy"), "La Nes")
+                new Question("Quel était le premier nom de Mario ?", "DIFFICILE", R.drawable.jumpman, 0, Arrays.asList("JumpMan", "Il s'est toujours appelé Mario", "Luigi"), "JumpMan"),
+                new Question("Quel est le nom de cette console ?", "DIFFICILE", R.drawable.famicom, 0, Arrays.asList("Famicom", "MegaDrive", "Nintendo 64"), "Famicom"),
+                new Question("Qui se cache sous cette armure ?", "DIFFICILE", R.drawable.samus, 0, Arrays.asList("Samus", "Luna", "Megaman"), "Samus"),
+                new Question("Au départ, que vendait Nintendo ?", "DIFFICILE", R.drawable.nintendo_first_logo, 0, Arrays.asList("Des jeux de cartes", "Des maquettes de voitures", "Des figurines"), "Des jeux de cartes"),
+                new Question("Qui a developpé les jeux Megaman ?", "DIFFICILE", R.drawable.megaman_title_screen, 0, Arrays.asList("Capcom", "Nintendo", "Sega"), "Capcom"),
+                new Question("De quel niveau de Mario Kart NES vient cette musique ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.mk_8_rainbow_road_snes, Arrays.asList("Route Arc-en-ciel", "Circuit Mario", "Parc Baby"), "Route Arc-en-ciel"),
+                new Question("De quel jeu vient cette musique ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.megaman2_dr_wilys_castle, Arrays.asList("Megaman", "Wii", "Nintendo 64"), "Megaman"),
+                new Question("Qui a composé l'ost des jeux a licence Zelda ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.gerudo_valley_zelda_oot, Arrays.asList("Koji Kondo", "Asuka Ota", "Junichi Masuda"), "Koji Kondo"),
+                new Question("Quelle console avait cette intoduction ?", "DIFFICILE", R.drawable.red_mushroom, R.raw.intro_nes, Arrays.asList("La Nes", "La Nintendo 64", "Les premières versions de la GameBoy"), "La Nes")
+                //Il manque une question
         };
 
         return new ArrayList<Question>(Arrays.asList(questions));
