fa077fde5c6ad8f506c50997074313fc33d6e0bb
Melanie V <melanie.vaubien@hotmail.com>
2020-03-19 11:16:19 +0100

    Set Game Activity

--- app/src/main/java/com/codingmel/nintendoquiz/MainActivity.java
+++ app/src/main/java/com/codingmel/nintendoquiz/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 
 import com.q42.android.scrollingimageview.ScrollingImageView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mp.stop();
-                Intent intent = new Intent(MainActivity.this,AboutActivity.class);
+                Intent intent = new Intent(MainActivity.this,GameActivity.class);
                 startActivity(intent);
             }
         });
@@ -54,5 +57,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private List<Question> getEasyQuestions(){
+        Question questions[] = new Question[]{
+                new Question("Quel est ce personnage ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Luigi","Wario","Waluigi"),"Waluigi"),
+                new Question("De quel jeu ce personnage est-il l'antagoniste ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("The Legend of Zelda","Pokemon","Super Mario Bros"),"The Legend of Zelda"),
+                new Question("Quel sont les noms de ces pokemons ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Bulbizarre, Salamèche et Carapuce","Germignon, Héricendre et Kaiminus","Brindibou, Flamiaou et Otaquin"),"Bulbizarre, Salamèche et Carapuce"),
+                new Question("De quel jeu vient cette musique ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("The Legend of Zelda","Mario Kart","Splatoon"),"Luigi"),
+                new Question("Quel est ce personnage ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Mario","Luigi","Wario"),"Luigi"),
+                new Question("De qui se personnage est-il le personnage mirroir?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Mario","Luigi","Peach"),"Mario"),
+                new Question("Dans quel lieu de Mario Bros retouve-t-on cette musique ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Les sous terrains","Sous L'eau","Dans les airs"),"Les sous terrains"),
+                new Question("De quel jeu vient cette musique ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Wii Sport","Mario Galaxy","Pokemon"),"Wii Sport"),
+                new Question("Quel est le nom de ce personnage ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Professeur Mario","Mario le Toubib","Dr.Mario"),"Dr.Mario"),
+                new Question("Quelle console avait cette intoduction ?","FACILE",R.drawable.red_mushroom,0, Arrays.asList("Game Cube","Wii","Nintendo 64"),"Game Cube")
+        };
+        return Arrays.asList(questions);
     }
 }
